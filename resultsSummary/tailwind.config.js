5b7eebd8b61e189d625f05bdd04c0d90d68366cb
roy-sourav23 <rsourav2023@gmail.com>
2024-03-12 19:00:34 +0530

    results summary

---
+++ resultsSummary/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      lightRed: "hsl(0, 100%, 67%)",
+      orangeyYellow: "hsl(39, 100%, 56%)",
+      greenTeal: "hsl(166, 100%, 37%)",
+      cobaltBlue: "hsl(234, 85%, 45%)",
+      lightSlateBlue: "hsl(252, 100%, 67%)",
+      lightRoyalBlue: "hsl(241, 81%, 54%)",
+      violetBlue: "hsla(256, 72%, 46%, 1)",
+      persianBlue: "hsla(241, 72%, 46%, 0)",
+      paleBlue: "hsl(221, 100%, 89%)",
+      lightLavender: "hsl(241, 100%, 89%)",
+      darkGrayBlue: "hsl(224, 30%, 27%)",
+    },
+    extend: {
+      fontFamily: {
+        hanken: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
